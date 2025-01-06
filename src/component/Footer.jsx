c29623880fdcc3e66848b9d9448b3297f3edfcd9
unknown <agehad248@gmail.com>
2025-01-06 10:55:21 +0200

    update

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='footer  text-center mt-5 text-white pb-3'>
+        <div className='footer  text-center text-white pb-3'>
             <p className='pt-3'>Copyright © 2024 Mexant , All Rights Reserved.</p>
            Designed by <span className='green fw-bold'> Ahmed Gehad</span>
         </div>
