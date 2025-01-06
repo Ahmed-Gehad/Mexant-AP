70d29dd12a414c737d890d5dd7aa2f04bee28212
unknown <agehad248@gmail.com>
2025-01-06 11:15:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navv from './component/Navv';
 import HomePage from './pages/HomePage';
-import Footer from './component/Footer';
 import AboutPage from './pages/AboutPage';
 import ServicesPage from './pages/ServicesPage';
 import ContactPage from './pages/ContactPage';
-
+import Footer from './component/Footer';
 
 function App() {
   return (
